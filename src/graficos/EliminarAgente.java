373856e593aab4b57b0604694885ecae916707d0
CarlosDubon <carlosminerodubon@gmail.com>
2017-07-06 15:38:30 -0600

    cxz

--- src/graficos/EliminarAgente.java
+++ src/graficos/EliminarAgente.java
@@ -19,12 +19,44 @@ public class EliminarAgente extends JFrame{
     private DBQuery DBase= new DBQuery();
     public EliminarAgente(){
         JPanel jpDelete = new JPanel();
-        GridLayout gl = new GridLayout(5,0,10,10); 
+        GridLayout gl = new GridLayout(4,2,10,10); 
+        JTextField txtBuscar = new JTextField(12);
+        JTextField txtNombre = new JTextField(12);
+        txtNombre.setEnabled(false);
+        JTextField txtApellido = new JTextField(12);
+        txtApellido.setEnabled(false);
+        JTextField txtPass = new JTextField(12);
+        txtPass.setEnabled(false);
+        
+        jpDelete.setLayout(gl);
+        jpDelete.add(new JLabel("Buscar: "));
+        jpDelete.add(txtBuscar);
+        jpDelete.add(new JLabel("Nombre: "));
+        jpDelete.add(txtNombre);
+        jpDelete.add(new JLabel("Apellido: "));
+        jpDelete.add(txtApellido);
+        jpDelete.add(new JLabel("Contaseña : "));
+        jpDelete.add(txtPass);
+    
+        JPanel jpBotones = new JPanel();
+        GridLayout glB = new GridLayout(1,3,5,5);
+        JButton btnCancel = new JButton("Cancelar");
+        JButton btnBuscar = new JButton("Buscar");
+        JButton btnEliminar = new JButton("Eliminar");
+        btnEliminar.setEnabled(false);
+        jpBotones.setLayout(glB);
+        jpBotones.add(btnCancel);
+        jpBotones.add(btnBuscar);
+        jpBotones.add(btnEliminar);
+                
         Container cp = getContentPane();
         FlowLayout fl = new FlowLayout();
+        cp.setLayout(fl);
+        cp.add(jpDelete);
+        cp.add(jpBotones);
         this.setLocation(((int)T1.getScreenSize().getWidth()/2)-125,(int)(T1.getScreenSize().getHeight()/2)-200);
         this.setTitle("ELIMINAR");
-        this.setSize(250, 375);
+        this.setSize(325, 225);
         this.setVisible(true);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
