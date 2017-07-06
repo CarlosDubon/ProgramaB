28e0dbc23726ffd6e587611d61e3979a43f9f606
CarlosDubon <carlosminerodubon@gmail.com>
2017-07-06 15:16:15 -0600

    a

--- src/graficos/EliminarAgente.java
+++ src/graficos/EliminarAgente.java
@@ -6,6 +6,7 @@
 package graficos;
 
 import database.DBQuery;
+import java.awt.*;
 import java.awt.Toolkit;
 import javax.swing.*;
 
@@ -17,8 +18,12 @@ public class EliminarAgente extends JFrame{
     private Toolkit T1= Toolkit.getDefaultToolkit();
     private DBQuery DBase= new DBQuery();
     public EliminarAgente(){
+        JPanel jpDelete = new JPanel();
+        GridLayout gl = new GridLayout(5,0,10,10); 
+        Container cp = getContentPane();
+        FlowLayout fl = new FlowLayout();
         this.setLocation(((int)T1.getScreenSize().getWidth()/2)-125,(int)(T1.getScreenSize().getHeight()/2)-200);
-        this.setTitle("LOGIN ADMIN");
+        this.setTitle("ELIMINAR");
         this.setSize(250, 375);
         this.setVisible(true);
         this.setResizable(false);
