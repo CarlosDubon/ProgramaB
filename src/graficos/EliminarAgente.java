abeed7fd7bd19da9a6170a024f10066851bc3386
CarlosDubon <carlosminerodubon@gmail.com>
2017-07-06 15:07:24 -0600

    sadasf

---
+++ src/graficos/EliminarAgente.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graficos;
+
+import database.DBQuery;
+import java.awt.Toolkit;
+import javax.swing.*;
+
+/**
+ *
+ * @author Carlos
+ */
+public class EliminarAgente extends JFrame{
+        private Toolkit T1= Toolkit.getDefaultToolkit();
+        private DBQuery DBase= new DBQuery();
+    public EliminarAgente(){
+        this.setLocation(((int)T1.getScreenSize().getWidth()/2)-125,(int)(T1.getScreenSize().getHeight()/2)-200);
+        this.setTitle("LOGIN ADMIN");
+        this.setSize(250, 375);
+        this.setVisible(true);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
