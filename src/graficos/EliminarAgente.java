83ffb01bb06acde83441871548287953fcbc7087
CarlosDubon <carlosminerodubon@gmail.com>
2017-07-06 15:15:22 -0600

    s

--- src/graficos/EliminarAgente.java
+++ src/graficos/EliminarAgente.java
@@ -14,8 +14,8 @@ import javax.swing.*;
  * @author Carlos
  */
 public class EliminarAgente extends JFrame{
-        private Toolkit T1= Toolkit.getDefaultToolkit();
-        private DBQuery DBase= new DBQuery();
+    private Toolkit T1= Toolkit.getDefaultToolkit();
+    private DBQuery DBase= new DBQuery();
     public EliminarAgente(){
         this.setLocation(((int)T1.getScreenSize().getWidth()/2)-125,(int)(T1.getScreenSize().getHeight()/2)-200);
         this.setTitle("LOGIN ADMIN");
