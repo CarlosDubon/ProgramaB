ce2b0e9ea262ff46334474227e79794b02af3c9d
CarlosDubon <carlosminerodubon@gmail.com>
2017-07-06 19:45:08 -0600

    clase

--- src/datos/LoginFail.java
+++ src/datos/LoginFail.java
@@ -5,6 +5,8 @@
  */
 package datos;
 
+import java.sql.Timestamp;
+
 /**
  *
  * @author Carlos
@@ -12,5 +14,47 @@ package datos;
 public class LoginFail {
     private String UserNick;
     private String UserPass;
+    private Timestamp Fecha; 
+
+    public LoginFail() {
+    }
+
+    public String getUserNick() {
+        return UserNick;
+    }
+
+    public String getUserPass() {
+        return UserPass;
+    }
+
+    /* public Timestamp getFecha() {
+    return Fecha;
+    }*/
+    public String getFecha(){
+        String date = ""+Fecha;
+        String sdate[] = date.split(" ");
+        date = sdate[0];
+        return date;
+    }
+        public String getHora(){
+        String date = ""+Fecha;
+        String sdate[] = date.split(" ");
+        date = sdate[1];
+        return date;
+    }
+
+    public void setUserNick(String UserNick) {
+        this.UserNick = UserNick;
+    }
+
+    public void setUserPass(String UserPass) {
+        this.UserPass = UserPass;
+    }
+
+    public void setFecha(Timestamp Fecha) {
+        this.Fecha = Fecha;
+    }
+    
+    
     
 }
